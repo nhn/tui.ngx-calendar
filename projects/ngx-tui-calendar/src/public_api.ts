06c4057b07b23fd50554aebe80784ec552c1b530
Bernardo Santos <bernardo.santos@ideavity.com>
2018-06-18 15:06:07 +0100

    added view select to example

--- projects/ngx-tui-calendar/src/public_api.ts
+++ projects/ngx-tui-calendar/src/public_api.ts
@@ -2,4 +2,4 @@
  * Public API Surface of ngx-tui-calendar
  */
 
-export * from './lib';
+export * from './lib/index';
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,22 +11,61 @@ export class AppComponent {
 
 	@ViewChild('calendar') calendar: NgxTuiCalendarComponent;
 
+
+	calendarViews = [
+		{ value: '0', name: 'month' },
+		{ value: '1', name: 'week' },
+		{ value: '2', name: 'day' }
+	];
+
 	constructor() {
 	}
 
+
+	onTuiCalendarCreated($event) {
+		this.calendar.createSchedules([
+			{
+				id: '1',
+				calendarId: '1',
+				title: 'my schedule',
+				category: 'time',
+				dueDateClass: '',
+				start: '2018-05-23T10:30:00+00:00',
+				end: '2018-05-23T12:30:00+00:00'
+			}
+		]);
+	}
+
 	onDate(date) {
-		//console.log(date);
+		console.log('onDate', date);
 	}
 
 	onTime(dateTime) {
-		//console.log(dateTime);
+		console.log('dateTime', dateTime);
 	}
 
 	onSchedule(schedule) {
-		//console.log(schedule);
+		console.log('schedule', schedule);
 	}
 
 	onDateChange($event) {
 		this.calendar.setDate(new Date($event.target.value));
 	}
+
+	onCalendarToday() {
+		this.calendar.today();
+	}
+
+	onCalendarNext() {
+		this.calendar.next();
+	}
+
+	onCalendarPrev() {
+		this.calendar.prev();
+	}
+
+	onChangeCalendarView($event) {
+		this.calendar.changeView(this.calendarViews.find(view => view.value === $event.target.value).name);
+	}
+
 }
