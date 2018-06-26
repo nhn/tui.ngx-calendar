781f86d6e4d62f343068040110f43f5a029d022f
Ian Worthington <worthy.vii@gmail.com>
2018-06-26 12:33:45 +0900

    Made some inputs of Schedule optional

--- projects/ngx-tui-calendar/src/lib/Models/Schedule.ts
+++ projects/ngx-tui-calendar/src/lib/Models/Schedule.ts
@@ -4,21 +4,21 @@ export interface Schedule {
   title: string
   start: string | Date
   end: string | Date
-  isAllDay: boolean
-  category: string
-  dueDateClass: string
-  location: string
-  attendees: Array<string>
-  recurrenceRule: any
-  isPending: boolean
-  isFocused: boolean
-  isVisible: boolean
-  isReadOnly: boolean
-  isPrivate: boolean
-  color: string
-  bgColor: string
-  dragBgColor: string
-  borderColor: string
-  customStyle: string
-  raw: any
+  isAllDay?: boolean
+  category?: string
+  dueDateClass?: string
+  location?: string
+  attendees?: Array<string>
+  recurrenceRule?: any
+  isPending?: boolean
+  isFocused?: boolean
+  isVisible?: boolean
+  isReadOnly?: boolean
+  isPrivate?: boolean
+  color?: string
+  bgColor?: string
+  dragBgColor?: string
+  borderColor?: string
+  customStyle?: string
+  raw?: any
 }
